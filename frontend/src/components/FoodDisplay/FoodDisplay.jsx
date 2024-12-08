5b154b9a88b55d9cda1fbf2330c94b6dec2d6f9b
shiva kumar gattu <gshiva0018@gmail.com>
2024-12-08 17:23:30 +0530

    Filltering based on category

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -15,8 +15,12 @@ const FoodDisplay = ({category}) => {
       <div className='food-display-list'>
          {
             food_list.map((item,index)=>{
+              if(category==="All"|| category===item.category){
+
                 return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
-            })
+       
+              }
+                     })
          }
       </div>
     </div>
