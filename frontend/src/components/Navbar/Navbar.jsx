3fad2fb74e91055a379a035ba5ddc8d4338bbe3c
shiva kumar gattu <gshiva0018@gmail.com>
2024-12-11 09:35:37 +0530

    applying lings

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
     <div className='navbar'>
      <img src={assets.logo} alt="logo" className='logo'/>
      <ul className='navbar-manu' >
-        <Link to="/" onClick={()=>setManu("home")} className={manu==="home"?"active":""} >Home </Link>
+        <Link to="/c" onClick={()=>setManu("home")} className={manu==="home"?"active":""} >Home </Link>
         <a href='#explore-menu'  onClick={()=>setManu("manu")} className={manu==="manu"?"active":""} >Manu</a>
         <a href='#app-download' onClick={()=>setManu("mobile-app")} className={manu==="mobile-app"?"active":""}>mobile-app</a>
         <a href='#footer' onClick={()=>setManu("contact-us")} className={manu==="contact-us"?"active":""} > contact us</a>
