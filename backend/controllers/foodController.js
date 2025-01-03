488c0cec15d0077ac24f42a17c32a71e00d6b606
shiva kumar gattu <gshiva0018@gmail.com>
2025-01-03 19:50:48 +0530

    food add

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -1,9 +1,27 @@
+import { console } from "inspector";
 import foodModel from "../models/foodmodel.js";
 import fs from "fs"
 
 //add food item 
 
 const addFood=async (req,res)=>{
+
+    let image_filename=`${req.file.filename}`;
+    const food= new foodModel({
+        name:req.body.name,
+        description:req.body.description,
+        price:req.body.price,
+        category:req.body.category,
+        image:image_filename
+    })
+    try {
+
+         await food.save()
+        
+    } catch (error) {
+        console.log(error)
+        
+    }
 
 }
 export {addFood}
